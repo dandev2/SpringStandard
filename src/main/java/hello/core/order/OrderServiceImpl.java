af9468a11d13a64baff8605afdd897622f7c92cf
dandev <dandev@naver.com>
2022-08-14 01:42:41 +0900

    Initial commit

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 단일 책임 원칙 잘 지킴. 할인 변경되면 할인만 변경하면 됨.
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
